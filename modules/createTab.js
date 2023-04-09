a6a1bddeaa96c362ce9b8b6268a3f26b292370bf
Irvin <irvindominguez1@gmail.com>
2023-04-09 16:08:42 -0600

    Refactored code, cleaner and more organized structure
    Broke down the original createTab() function into smaller functions to improve readability and maintainability. The handleTabCompletion(), handlePageNotFound(), handleProfileScraping(), and logProfileData() functions handle different parts of the logic, and console logs are still included for debugging purposes.

--- modules/createTab.js
+++ modules/createTab.js
@@ -1,4 +1,3 @@
-// Implement the logic for scraping LinkedIn data
 console.log('Content script loaded.');
 
 // Create a tab and inject the content script
@@ -8,59 +7,70 @@ export function createTab(url) {
       console.log('Opening url...');
       chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
         if (info.status === 'complete' && tabId === createdTab.id) {
-          chrome.tabs.get(tabId, (tab) => {
-            
-            // Check for 404 and move to next profile 
-            if (tab.url.includes('/404')) {
-              console.log('Page not found (404). Moving to the next profile...');
-              // Close the current tab and reject the promise
-              chrome.tabs.remove(tabId);
-              reject(new Error('Page not found (404).'));
-            } else {
-              chrome.tabs.onUpdated.removeListener(listener);
-
-              console.log('Injecting script...');
-              
-              chrome.scripting.executeScript(
-                {
-                  target: { tabId: createdTab.id },
-                  files: ['modules/linkedinProfileScrapper.js'],
-                },
-                (injectionResults) => {
-                  if (chrome.runtime.lastError) {
-                    console.error(chrome.runtime.lastError);
-                    reject(chrome.runtime.lastError);
-                    return;
-                  }
-                  console.log('Script injected:', injectionResults);
-
-                  const port = chrome.tabs.connect(tabId, { name: 'linkedinProfileScrapper' });
-                  port.postMessage({ action: 'scrapeLinkedInProfile' });
-
-                  port.onMessage.addListener((response) => {
-                    console.log('Name:', response.fullName);
-                    console.log('Bio:', response.tagLine);
-                    console.log('Location:', response.contactLocation);
-                    console.log('About:', response.contactAbout);
-                    console.log('Positions:', response.positions);
-
-                    resolve({ profileData: response, createdTab });
-                  });
-
-                  port.onDisconnect.addListener((port) => {
-                    if (chrome.runtime.lastError) {
-                      console.error(chrome.runtime.lastError);
-                      reject(chrome.runtime.lastError);
-                      return;
-                    }
-                  });
-                }
-              );
-            }
-          });
+          handleTabCompletion(tabId, listener, resolve, reject);
         }
       });
     });
   });
 }
 
+function handleTabCompletion(tabId, listener, resolve, reject) {
+  chrome.tabs.get(tabId, (tab) => {
+    if (tab.url.includes('/404')) {
+      handlePageNotFound(tabId, listener, reject);
+    } else {
+      handleProfileScraping(tabId, listener, resolve, reject);
+    }
+  });
+}
+
+function handlePageNotFound(tabId, listener, reject) {
+  console.log('Page not found (404). Moving to the next profile...');
+  chrome.tabs.onUpdated.removeListener(listener);
+  chrome.tabs.remove(tabId);
+  reject(new Error('Page not found (404).'));
+}
+
+function handleProfileScraping(tabId, listener, resolve, reject) {
+  chrome.tabs.onUpdated.removeListener(listener);
+  console.log('Injecting script...');
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tabId },
+      files: ['modules/linkedinProfileScrapper.js'],
+    },
+    (injectionResults) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        reject(chrome.runtime.lastError);
+        return;
+      }
+      console.log('Script injected:', injectionResults);
+
+      const port = chrome.tabs.connect(tabId, { name: 'linkedinProfileScrapper' });
+      port.postMessage({ action: 'scrapeLinkedInProfile' });
+
+      port.onMessage.addListener((response) => {
+        logProfileData(response);
+        resolve({ profileData: response, createdTab: tabId });
+      });
+
+      port.onDisconnect.addListener((port) => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          reject(chrome.runtime.lastError);
+          return;
+        }
+      });
+    }
+  );
+}
+
+function logProfileData(response) {
+  console.log('Name:', response.fullName);
+  console.log('Bio:', response.tagLine);
+  console.log('Location:', response.contactLocation);
+  console.log('About:', response.contactAbout);
+  console.log('Positions:', response.positions);
+}
